31e2e5526c45f1bc2a70f8425a83b9c51082b2ef
Leo Michel <bob.bobby@outlook.de>
2020-03-13 21:28:17 +0100

    v0.0.1
    kann jetzt einen Text einlesen, daraus ein Alphabet bauen und die Whrscheinlichkeit der einzelnen Buchstaben berechnen

--- kompressorunit.pas
+++ kompressorunit.pas
@@ -16,6 +16,7 @@ uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Buttons;
 
 type
+  Tarrayofreal= array of real;
 
   { TKompressorForm }
 
@@ -31,6 +32,8 @@ type
     OpenSpeedButton: TSpeedButton;
     SaveSpeedButton: TSpeedButton;
     TopLabel: TLabel;
+    procedure KomprimierenButtonClick(Sender: TObject);
+    //procedure einlesen(data:string);
   private
 
   public
@@ -42,7 +45,91 @@ var
 
 implementation
 
+function aofrealtostr(a:Tarrayofreal):string;   //schreibt ein array of real in einen String um
+var
+  i:int64;
+  begin
+    result:='';
+    for i:=0 to (length(a)-1) do begin
+      result:=result+ floattostr(a[i])+' ';
+      end;
+    end;
+
+function instring(s:string; a:char):boolean;     //prüfen ob ein Buchstabe in einem String ist
+var
+  i:int64;
+  begin
+    result:=false;
+    for i:=1 to length(s) do begin
+      if s[i]=a then begin
+        result:=true;
+        break;
+      end;
+    end;
+  end;
+
+function getalpha(s:string):string;       //generiert das Alphabet, das s nutzt
+var
+  i:int64;
+  alpha:string;
+  begin
+    alpha:='';
+    for i:=1 to length(s) do begin
+      if not instring(alpha,s[i]) then alpha:=alpha+s[i];                       //Buchstaben in alpha aufnehmen, wenn noch nicht vorhanden
+    end;
+    result:=alpha;
+  end;
+
+function getwahrsch(s,alpha:string):Tarrayofreal;
+var
+  i,n,lenge:int64;
+  wahrsch: array of real;
+  begin
+    lenge:=length(s);
+    setlength(wahrsch,length(alpha));
+    for i:=1 to length(alpha) do begin
+      for n:=1 to lenge do begin
+        if alpha[i]=s[n] then wahrsch[i-1]:= wahrsch[i-1]+(1/lenge);    //für jeden gefunden Buchstaben aus alpha in s
+      end;                                                          //an dessen Stelle in wahrsch die Wahrscheinlichkeit
+    end;                                                            //aufsummieren
+    result:=copy(wahrsch);
+  end;
+
 {$R *.lfm}
+
+{ TKompressorForm }
+
+procedure TKompressorForm.KomprimierenButtonClick(Sender: TObject);
+var
+  Data,alpha:string;
+  wahrsch:array of real;
+  summe:real;
+  i:int64;
+begin
+  data:='';
+
+  for i:=0 to (Memo.Lines.count-1) do data:=data+ Memo.lines[i];       //Text aus Memo einlesen
+
+  alpha:=getalpha(Data);
+  showmessage('alphaLÄ: '+inttostr(length(alpha))+' DataLÄ: '+inttostr(length(Data)));
+  wahrsch:=copy(getwahrsch(Data,alpha));
+  Memo.lines.add('Alphabet: '+alpha);
+  Memo.lines.add('Wahrscheinlichkeit: '+aofrealtostr(wahrsch));
+
+  summe:=0;
+  for i:=0 to (length(wahrsch)-1) do begin
+    summe:=summe+wahrsch[i];
+    end;
+  Memo.lines.add('Summe der Wahrscheinlichkeiten: '+floattostr(summe));
+
+end;
+
+{procedure TKompressorform.einlesen(data:string);
+var
+  i:int64;
+  begin
+    for i:=0 to Memo.Lines.count do data:=data+ Memo.lines[i];
+  end;      }
 
 { TKompressorForm }
 
